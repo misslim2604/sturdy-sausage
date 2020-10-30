ec36a42c044be81e3eeb20242bda4e6d418e831b
Mariè Lim <72059326+misslim2604@users.noreply.github.com>
2020-10-30 09:53:34 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import Style from './Style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
